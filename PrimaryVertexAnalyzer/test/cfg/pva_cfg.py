b491a3fbd3f16d0d54fc086193e4c2b26c5c0ff6
Marino Missiroli <marino.missiroli@cern.ch>
2021-05-13 19:11:50 +0200

    small updates to test/cfg/pva_cfg.py

--- PrimaryVertexAnalyzer/test/cfg/pva_cfg.py
+++ PrimaryVertexAnalyzer/test/cfg/pva_cfg.py
@@ -105,10 +105,14 @@ from usercode.PrimaryVertexAnalyzer.utils.common import *
 import glob
 import os
 
-EXE(cmsDriverCmd+' --python_filename tmp_cfg.py')
+# create, load and remove the configuration file
+EXE(cmsDriverCmd+' --python_filename '+os.path.dirname(__file__)+'/tmp_cfg.py')
 from tmp_cfg import cms, process
-for _tmpf in glob.glob('tmp_cfg.py*'):
-  os.remove(_tmpf)
+for _tmpf in glob.glob(os.path.dirname(__file__)+'/tmp_cfg.py*'): os.remove(_tmpf)
+
+# use all input files of the dataset
+from usercode.PrimaryVertexAnalyzer.utils.das import *
+process.source.fileNames = files_from_dataset(dataset_from_file(process.source.fileNames[0]))
 
 # remove OutputModules of base configuration
 for _modname in process.outputModules_():
@@ -122,9 +126,9 @@ for _modname in process.outputModules_():
 ### analysis sequence
 ###
 nevent=-1
-info = "test"
+info = 'test'
 debug = False
-eventsToProcess = "" #cms.untracked.VEventRange('1:4474')
+eventsToProcess = '' #cms.untracked.VEventRange('1:4474')
 zdumpcenter = 0
 zdumpwidth = 100.
 use_tp = True
@@ -132,127 +136,127 @@ use_hlt = False
 use_2file_solution = False
 use_lumiInfo = True
 
-vcollections = ["offlinePrimaryVertices", "offlinePrimaryVerticesWithBS"]
+vcollections = ['offlinePrimaryVertices', 'offlinePrimaryVerticesWithBS']
 
-trkTimesLabel = "tofPID:t0safe"
-trkTimeResosLabel = "tofPID:sigmat0safe"
+trkTimesLabel = 'tofPID:t0safe'
+trkTimeResosLabel = 'tofPID:sigmat0safe'
 
 parameters={  # can be overwritten by arguments of the same name
-  "4D": cms.untracked.bool(True),
-  "selNdof": cms.untracked.double(4.0),
-  "selNdofWithBS": cms.untracked.double(2.0),
-#  "splitmethod":cms.untracked.int32(0),
-  "usefit":cms.untracked.bool(False),
-  "use_tp":cms.untracked.bool(True),
-  "use2file":cms.untracked.bool(False),
-#  "ptrunc":cms.untracked.double(1.e-50),
-  "fill_track_histos":cms.untracked.bool(False),
-  "fplo":cms.untracked.double(0),
-  "chi2cutoff":cms.double(2.5), 
-  "verboseAnalyzer":cms.untracked.bool(False),
-  "verboseProducer":cms.untracked.bool(False),
-  "verboseClusterizer":cms.untracked.bool(False),
-  "verboseClusterizer2D":cms.untracked.bool(False),
-  "zdumpcenter" : cms.untracked.double(0.0),
-  "zdumpwidth" : cms.untracked.double(20.0),
-  "reco":cms.untracked.bool(False),
-  "autodump":cms.untracked.int32(0),
-  "nDump":cms.untracked.int32(0),
-  "nDumpTracks":cms.untracked.int32(0),
-#  "mintrkweight":cms.untracked.double(0.5),
-  "uniquetrkweight":cms.double(0.8),
-  "uniquetrkminp":cms.double(0.0),
-  "zmerge":cms.double(1.e-2),
-  "coolingFactor":cms.double(0.6),
-  "Tmin": cms.double(2.0),
-  "Tpurge":cms.double(2.0),
-  "Tstop": cms.double(0.5),
-  "vertexSize" : cms.double(0.006),
-  "vertexSizeTime" : cms.double(0.008),
-  "d0CutOff" : cms.double(3.),
-  "dzCutOff" : cms.double(3.),
-  "zrange" : cms.double(4),
-  "convergence_mode" : cms.int32(0),
-  "delta_lowT" : cms.double(1.e-3),
-  "delta_highT" : cms.double(1.e-2),
+  '4D': cms.untracked.bool(True),
+  'selNdof': cms.untracked.double(4.0),
+  'selNdofWithBS': cms.untracked.double(2.0),
+#  'splitmethod': cms.untracked.int32(0),
+  'usefit': cms.untracked.bool(False),
+  'use_tp': cms.untracked.bool(True),
+  'use2file': cms.untracked.bool(False),
+#  'ptrunc': cms.untracked.double(1.e-50),
+  'fill_track_histos': cms.untracked.bool(False),
+  'fplo': cms.untracked.double(0),
+  'chi2cutoff': cms.double(2.5),
+  'verboseAnalyzer': cms.untracked.bool(False),
+  'verboseProducer': cms.untracked.bool(False),
+  'verboseClusterizer': cms.untracked.bool(False),
+  'verboseClusterizer2D': cms.untracked.bool(False),
+  'zdumpcenter': cms.untracked.double(0.0),
+  'zdumpwidth': cms.untracked.double(20.0),
+  'reco': cms.untracked.bool(False),
+  'autodump': cms.untracked.int32(0),
+  'nDump': cms.untracked.int32(0),
+  'nDumpTracks': cms.untracked.int32(0),
+#  'mintrkweight': cms.untracked.double(0.5),
+  'uniquetrkweight': cms.double(0.8),
+  'uniquetrkminp': cms.double(0.0),
+  'zmerge': cms.double(1.e-2),
+  'coolingFactor': cms.double(0.6),
+  'Tmin': cms.double(2.0),
+  'Tpurge': cms.double(2.0),
+  'Tstop': cms.double(0.5),
+  'vertexSize': cms.double(0.006),
+  'vertexSizeTime': cms.double(0.008),
+  'd0CutOff': cms.double(3.),
+  'dzCutOff': cms.double(3.),
+  'zrange': cms.double(4),
+  'convergence_mode': cms.int32(0),
+  'delta_lowT': cms.double(1.e-3),
+  'delta_highT': cms.double(1.e-2),
 # track selection
-  "maxNormalizedChi2":cms.double(10.0),
-  "minPixelLayersWithHits":cms.int32(2),
-  "minSiliconLayersWithHits":cms.int32(5),
-  "maxD0Significance":cms.double(4.0), 
-  "minPt":cms.double(0.0),
-  "maxEta":cms.double(4.0),
-  "trackQuality":cms.string("any"),
+  'maxNormalizedChi2': cms.double(10.0),
+  'minPixelLayersWithHits': cms.int32(2),
+  'minSiliconLayersWithHits': cms.int32(5),
+  'maxD0Significance': cms.double(4.0),
+  'minPt': cms.double(0.0),
+  'maxEta': cms.double(4.0),
+  'trackQuality': cms.string('any'),
 # track selection, experimental
-  "maxDzError":cms.double(1.0),
-  "maxD0Error":cms.double(1.0),
+  'maxDzError': cms.double(1.0),
+  'maxD0Error': cms.double(1.0),
 # vertex selection
-  "minNdof": cms.double( 0.0 ),
+  'minNdof': cms.double(0.0),
 # temp
-  "trackTimeQualityThreshold" : cms.untracked.double(0.8),
-  "purge" : cms.untracked.int32(0),
-  "rho0mode" : cms.untracked.int32(0),  # /nt, as before
-  "mergenotc" : cms.untracked.bool(False),
-  "mergeafterpurge" : cms.untracked.bool(False),
-  "fillzmerge" : cms.untracked.double(0.0060), # default = vertexSize 
-  "use_hitpattern" : cms.untracked.int32(0),
-  "use_pt" : cms.untracked.int32(0)
+  'trackTimeQualityThreshold': cms.untracked.double(0.8),
+  'purge': cms.untracked.int32(0),
+  'rho0mode': cms.untracked.int32(0),  # /nt, as before
+  'mergenotc': cms.untracked.bool(False),
+  'mergeafterpurge': cms.untracked.bool(False),
+  'fillzmerge': cms.untracked.double(0.0060), # default = vertexSize
+  'use_hitpattern': cms.untracked.int32(0),
+  'use_pt': cms.untracked.int32(0)
 }
 
 tkFilterParameters = process.unsortedOfflinePrimaryVertices.TkFilterParameters.clone()
-print "original trackFilterParameters (z-clustering)"
+print 'original trackFilterParameters (z-clustering)'
 print tkFilterParameters
 for par_name in [
-  "maxNormalizedChi2",
-  "minPixelLayersWithHits",
-  "minSiliconLayersWithHits",
-  "maxD0Significance",
-  "maxD0Error",
-  "maxDzError",
-  "minPt",
-  "maxEta",
-  "trackQuality",
+  'maxNormalizedChi2',
+  'minPixelLayersWithHits',
+  'minSiliconLayersWithHits',
+  'maxD0Significance',
+  'maxD0Error',
+  'maxDzError',
+  'minPt',
+  'maxEta',
+  'trackQuality',
 ]:
   try:
     default =  getattr(tkFilterParameters, par_name)
     if default != parameters[par_name]:
-      print "changing tkFilter parameter ", par_name, " from ", default, "  to ", parameters[par_name]
+      print 'changing tkFilter parameter ', par_name, ' from ', default, '  to ', parameters[par_name]
     setattr(tkFilterParameters, par_name, parameters[par_name])
   except ValueError:
-    print "pvAnalysisOnly_cfg: attribute tkFilterParameters.", par_name , " not found"
+    print 'pvAnalysisOnly_cfg: attribute tkFilterParameters.', par_name , ' not found'
 
 # analysis
-process.vertexAnalyzer = cms.EDAnalyzer("PrimaryVertexAnalyzer4PU",
+process.vertexAnalyzer = cms.EDAnalyzer('PrimaryVertexAnalyzer4PU',
   info = cms.untracked.string(info),
-  f4D = parameters["4D"],
-  selNdof = parameters["selNdof"],                                    
-  selNdofWithBS = parameters["selNdofWithBS"],                                    
+  f4D = parameters['4D'],
+  selNdof = parameters['selNdof'],
+  selNdofWithBS = parameters['selNdofWithBS'],
   beamSpot = cms.InputTag('offlineBeamSpot'),
-  simG4 = cms.InputTag("g4SimHits"),
-  outputFile = cms.untracked.string("pv.root"),
-  verbose = parameters["verboseAnalyzer"],
+  simG4 = cms.InputTag('g4SimHits'),
+  outputFile = cms.untracked.string(opts.outputFile),
+  verbose = parameters['verboseAnalyzer'],
   veryverbose = cms.untracked.bool(False),
-  recoTrackProducer = cms.untracked.string("generalTracks"),
+  recoTrackProducer = cms.untracked.string('generalTracks'),
   zmatch = cms.untracked.double(0.05),
-  autodump = parameters["autodump"],
-  nDump = parameters["nDump"],
-  nDumpTracks = parameters["nDumpTracks"],
-  RECO = parameters["reco"],
-  use_tp = parameters["use_tp"],
-  fill_track_histos = parameters["fill_track_histos"],
+  autodump = parameters['autodump'],
+  nDump = parameters['nDump'],
+  nDumpTracks = parameters['nDumpTracks'],
+  RECO = parameters['reco'],
+  use_tp = parameters['use_tp'],
+  fill_track_histos = parameters['fill_track_histos'],
   track_timing = cms.untracked.bool(True),
   TkFilterParameters = tkFilterParameters,
-  trackingParticleCollection = cms.untracked.InputTag("mix", "MergedTrackTruth"),
-  trackingVertexCollection = cms.untracked.InputTag("mix", "MergedTrackTruth"),
-  trackAssociatorMap = cms.untracked.InputTag("trackingParticleRecoTrackAsssociation"),
-  TrackTimesLabel = cms.untracked.InputTag(trkTimesLabel),  
+  trackingParticleCollection = cms.untracked.InputTag('mix', 'MergedTrackTruth'),
+  trackingVertexCollection = cms.untracked.InputTag('mix', 'MergedTrackTruth'),
+  trackAssociatorMap = cms.untracked.InputTag('trackingParticleRecoTrackAsssociation'),
+  TrackTimesLabel = cms.untracked.InputTag(trkTimesLabel),
   TrackTimeResosLabel = cms.untracked.InputTag(trkTimeResosLabel),
-#  TrackTimesLabel = cms.untracked.InputTag("tofPID:t0safe"),  
-#  TrackTimeResosLabel = cms.untracked.InputTag("tofPID:sigmat0safe"),
-  TrackTimeQualityMapLabel = cms.untracked.InputTag("mtdTrackQualityMVA:mtdQualMVA"),
-  TrackTimeQualityThreshold = cms.untracked.double( parameters["trackTimeQualityThreshold"].value()),
-  vertexAssociator = cms.untracked.InputTag("VertexAssociatorByPositionAndTracks"),
-  lumiInfoTag = cms.untracked.InputTag("LumiInfo", "brilcalc"),
+#  TrackTimesLabel = cms.untracked.InputTag('tofPID:t0safe'),
+#  TrackTimeResosLabel = cms.untracked.InputTag('tofPID:sigmat0safe'),
+  TrackTimeQualityMapLabel = cms.untracked.InputTag('mtdTrackQualityMVA:mtdQualMVA'),
+  TrackTimeQualityThreshold = cms.untracked.double(parameters['trackTimeQualityThreshold'].value()),
+  vertexAssociator = cms.untracked.InputTag('VertexAssociatorByPositionAndTracks'),
+  lumiInfoTag = cms.untracked.InputTag('LumiInfo', 'brilcalc'),
   useVertexFilter = cms.untracked.bool(False),
   compareCollections = cms.untracked.int32(0),
   vertexRecoCollections = cms.VInputTag(vcollections),
@@ -260,8 +264,8 @@ process.vertexAnalyzer = cms.EDAnalyzer("PrimaryVertexAnalyzer4PU",
 
 process.setSchedule_(cms.Schedule())
 
-if parameters["use_tp"]:
-  process.load("Validation.RecoTrack.TrackValidation_cff")
+if parameters['use_tp']:
+  process.load('Validation.RecoTrack.TrackValidation_cff')
   process.theTruth = cms.Sequence(
       process.tpClusterProducer
     + process.quickTrackAssociatorByHits
