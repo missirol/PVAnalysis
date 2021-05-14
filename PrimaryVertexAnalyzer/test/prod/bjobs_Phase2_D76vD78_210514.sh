8a468dc38f7e0f4c572b3780b9615b86f5313a49
Marino Missiroli <marino.missiroli@cern.ch>
2021-05-14 16:23:24 +0200

    added test/prod/bjobs_Phase2_D76vD78_210514.sh

---
+++ PrimaryVertexAnalyzer/test/prod/bjobs_Phase2_D76vD78_210514.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+
+if [ $# -lt 1 ]; then
+  printf "\n%s\n\n" ">> argument missing - specify path to output directory"
+  exit 1
+fi
+
+NEVT=-1
+ODIR=${1}
+
+if [ -d ${ODIR} ]; then
+  printf "%s\n" "output directory already exists: ${ODIR}"
+  exit 1
+fi
+
+declare -A samplesMap
+
+samplesMap["RelValTenMuExtendedE0To200_mcRun4_D76_NoPU"]="/RelValTenMuExtendedE_0_200/CMSSW_11_3_0_pre6-113X_mcRun4_realistic_v6_2026D76noPU-v1/GEN-SIM-RECO"
+samplesMap["RelValTenMuExtendedE0To200_mcRun4_D78_NoPU"]="/RelValTenMuExtendedE_0_200/CMSSW_11_3_0_pre6-113X_mcRun4_realistic_v6_2026D78noPU-v1/GEN-SIM-RECO"
+
+recoKeys=(
+  Phase2_D76
+  Phase2_D78
+)
+
+for recoKey in "${recoKeys[@]}"; do
+  python ${CMSSW_BASE}/src/usercode/PrimaryVertexAnalyzer/test/cfg/pva_cfg.py \
+    dumpPython=.tmp_${recoKey}_cfg.py numThreads=1 reco=${recoKey}
+
+  for sampleKey in ${!samplesMap[@]}; do
+
+    if [[ ${recoKey} == *D76* ]] && [[ ${sampleKey} != *D76* ]]; then continue; fi;
+    if [[ ${recoKey} == *D78* ]] && [[ ${sampleKey} != *D78* ]]; then continue; fi;
+
+    sampleName=${samplesMap[${sampleKey}]}
+
+    # number of events per sample
+    numEvents=${NEVT}
+#    if [[ ${sampleKey} == *MinBias* ]]; then
+#      numEvents=2000000
+#    fi
+
+    bdriver -c .tmp_${recoKey}_cfg.py --customize-cfg -m ${numEvents} \
+      -n 500 --cpus 1 --mem 1G --time 00:20:00 \
+      -d ${sampleName} -p 0 -o ${ODIR}/${recoKey}/${sampleKey}
+
+    rm -f .tmp_${recoKey}_cfg.py
+  done
+  unset sampleKey numEvents sampleName
+
+done
+unset recoKey recoKeys samplesMap NEVT ODIR
